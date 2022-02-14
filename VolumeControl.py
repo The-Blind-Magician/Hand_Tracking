f355fa89c2e89fa8a63aae5f6da43ca214d4d0b5
The-Blind-Magician <christian220h@outlook.com>
2022-02-14 03:36:31 -0600

    Basic functionality

--- VolumeControl.py
+++ VolumeControl.py
@@ -1,8 +1,13 @@
+import math
+
 import cv2
 import mediapipe as mp
 import time
 import numpy as np
 import HandTrackingModule as htm
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume, IAudioEndpointVolume
 
 WRIST, THUMB_1, THUMB_2, THUMB_3, THUMB_4, INDEX_1, INDEX_2, INDEX_3, INDEX_4, MIDDLE_1, MIDDLE_2, MIDDLE_3, MIDDLE_4 = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
 RING_1, RING_2, RING_3, RING_4, PINKY_1, PINKY_2, PINKY_3, PINKY_4 = 13, 14, 15, 16, 17, 18, 19, 20
@@ -10,9 +15,10 @@ RING_1, RING_2, RING_3, RING_4, PINKY_1, PINKY_2, PINKY_3, PINKY_4 = 13, 14, 15,
 def round_to_multiple(number, multiple):
     return multiple * round(number/multiple)
 
-def get_distance_midpoint(x, y, multiple):
-    dist = int(np.linalg.norm(np.array(x) - np.array(y)))
-    dist = round_to_multiple(dist, multiple)
+def get_distance_midpoint(x, y, multiple=0):
+    dist = np.linalg.norm(np.array(x) - np.array(y))
+    if multiple != 0:
+        dist = round_to_multiple(dist, multiple)
     midpoint = [int((x[0] + y[0]) / 2), int((x[1] + y[1]) / 2)]
     return dist, midpoint
 
@@ -23,11 +29,30 @@ def draw_line(img, lmList, a, b, color):
 def get_two_points(lmList, a, b):
     return [lmList[a][1], lmList[a][2]], [lmList[b][1], lmList[b][2]]
 
+def set_volume(varDist, scaleDist, volume):
+    #factor = scaleDist / 200
+    #a = round_to_multiple((factor * 30), 1)
+    #b = round_to_multiple((factor * 250), 1)
+    varDist = np.interp(varDist, [40, 240], [1, 100])
+    vol = 20*math.log10(varDist)
+    #print(vol)
+    vol = np.interp(vol, [0, 40], [-65, 0])
+    #vol = round_to_multiple(vol, 0.1)
+    #print(vol)
+
+    cv2.putText(img, "Volume: " + str(int(np.interp(vol, [-65, 0], [0, 100]))), (5, 700), cv2.FONT_HERSHEY_PLAIN, 2,
+                (detector.mpDraw.BLUE_COLOR), 2)
+    volume.SetMasterVolumeLevel(vol, None)
+
 wCam, hCam = 1280, 720
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 ptime = 0
 detector = htm.handDetection()
@@ -46,19 +71,21 @@ while True:
 
     if len(hand1) != 0:
         a1, b1 = get_two_points(hand1, THUMB_4, INDEX_4)
+        a3, b3 = get_two_points(hand1, MIDDLE_1, MIDDLE_2)
         draw_line(img, hand1, THUMB_4, INDEX_4, detector.mpDraw.RED_COLOR)
-        dist1, midpoint1 = get_distance_midpoint(a1, b1, 5)
+        dist1, midpoint1 = get_distance_midpoint(a1, b1)
+        dist3, midpoint3 = get_distance_midpoint(a3, b3)
 
-        cv2.putText(img, str(dist1), (midpoint1[0], midpoint1[1]), cv2.FONT_HERSHEY_PLAIN, 1, (detector.mpDraw.BLUE_COLOR), 2)
-        cv2.circle(img, (midpoint1[0], midpoint1[1]), 2, detector.mpDraw.RED_COLOR, 3)
+        dist1 = round_to_multiple(dist1, 5)
+        set_volume(dist1, dist3, volume)
+        cv2.putText(img, str(dist1), (midpoint1[0]-70, midpoint1[1]), cv2.FONT_HERSHEY_PLAIN, 2, (detector.mpDraw.BLUE_COLOR), 3)
+        cv2.circle(img, (midpoint1[0], midpoint1[1]), 2, detector.mpDraw.RED_COLOR, 2)
 
     if len(hand2) != 0:
         a2, b2 = get_two_points(hand2, THUMB_4, INDEX_4)
-        a3, b3 = get_two_points(hand2, INDEX_1, WRIST)
         draw_line(img, hand2, THUMB_4, INDEX_4, detector.mpDraw.RED_COLOR)
         dist2, midpoint2 = get_distance_midpoint(a2, b2, 5)
 
-        dist3, midpoint3 = get_distance_midpoint(a3, b3, 5)
         cv2.putText(img, str(dist3), (midpoint3[0], midpoint3[1]), cv2.FONT_HERSHEY_PLAIN, 1, (detector.mpDraw.BLUE_COLOR), 2)
 
         cv2.putText(img, str(dist2), (midpoint2[0], midpoint2[1]), cv2.FONT_HERSHEY_PLAIN, 1, (detector.mpDraw.BLUE_COLOR), 2)
